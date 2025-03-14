ba9d78da8b390dd3af37d1f84b9e30bb00639587
Valchap <valchap616@gmail.com>
2025-03-14 22:10:58 +0100

    Ignore dist folder from git

--- marmalade/src/font.rs
+++ marmalade/src/font.rs
@@ -2,7 +2,9 @@ use meshtext::MeshGenerator;
 
 pub const MONOGRAM: &[u8] = include_bytes!("../resources/fonts/monogram-extended.ttf");
 
+pub type Font = MeshGenerator<meshtext::Face<'static>>;
+
 #[must_use]
-pub fn from_bytes(bytes: &'static [u8]) -> MeshGenerator<meshtext::Face<'static>> {
+pub fn from_bytes(bytes: &'static [u8]) -> Font {
     MeshGenerator::new(bytes)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,64 @@ use glam::Vec2;
 use marmalade::dom_stack;
 use marmalade::draw_scheduler;
 use marmalade::font;
+use marmalade::font::Font;
 use marmalade::input;
 use marmalade::input::Key;
 use marmalade::render::canvas2d::Canvas2d;
 use marmalade::render::canvas2d::DrawTarget2d;
 use marmalade::render::color;
 use marmalade::tick_scheduler::TickScheduler;
+use std::cell::RefCell;
 use std::time::Duration;
 
 mod entity;
 mod world;
 
+fn game_tick(position: &mut Vec2) {
+    if input::is_key_down(Key::A) {
+        position.x -= 0.5;
+    }
+    if input::is_key_down(Key::D) {
+        position.x += 0.5;
+    }
+    if input::is_key_down(Key::S) {
+        position.y -= 0.5;
+    }
+    if input::is_key_down(Key::W) {
+        position.y += 0.5;
+    }
+}
+
+fn render_tick(canvas: &mut Canvas2d, font: &mut Font, position: Vec2) {
+    canvas.fit_screen();
+
+    canvas.pixel_perfect_view();
+
+    canvas.clear(color::rgb(0., 0., 0.));
+
+    canvas.draw_regular(
+        position,
+        100.,
+        64,
+        color::rgb(1., 0.5, 0.5),
+        &canvas.white_texture(),
+    );
+
+    canvas.draw_text(
+        Vec2::new(100., 100.),
+        50.,
+        "Welcome to the pool",
+        font,
+        color::rgb(1., 1., 1.),
+        &canvas.white_texture(),
+    );
+
+    canvas.flush();
+}
+
 async fn async_main() {
+    let a = RefCell::new(String::new());
+
     dom_stack::set_title("Slime Pool");
 
     let main_canvas = dom_stack::create_full_screen_canvas();
@@ -30,44 +76,10 @@ async fn async_main() {
 
     draw_scheduler::set_on_draw(move || {
         for _ in 0..tick_scheduler.tick_count() {
-            if input::is_key_down(Key::A) {
-                position.x -= 0.5;
-            }
-            if input::is_key_down(Key::D) {
-                position.x += 0.5;
-            }
-            if input::is_key_down(Key::S) {
-                position.y -= 0.5;
-            }
-            if input::is_key_down(Key::W) {
-                position.y += 0.5;
-            }
+            game_tick(&mut position);
         }
 
-        canvas.fit_screen();
-
-        canvas.pixel_perfect_view();
-
-        canvas.clear(color::rgb(0., 0., 0.));
-
-        canvas.draw_regular(
-            position,
-            100.,
-            64,
-            color::rgb(1., 0.5, 0.5),
-            &canvas.white_texture(),
-        );
-
-        canvas.draw_text(
-            Vec2::new(100., 100.),
-            50.,
-            "Welcome to the pool",
-            &mut font,
-            color::rgb(1., 1., 1.),
-            &canvas.white_texture(),
-        );
-
-        canvas.flush();
+        render_tick(&mut canvas, &mut font, position);
     });
 }
 
